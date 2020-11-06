4becbb95747d9ce2809bc48a353b4fb5136b96a3
edendahary <57237141+edendahary@users.noreply.github.com>
2020-11-06 12:13:04 +0200

    Update Graph_DS.java

--- ex0/Graph_DS.java
+++ ex0/Graph_DS.java
@@ -5,13 +5,16 @@ import java.util.*;
 public class Graph_DS implements graph {
     private int ES;
     private int MC;
-  //  private final List<node_data> nodeDataList = new ArrayList<>();
     private  Map<Integer,node_data> MapNode;
+
+
+    //create a new graph
     public Graph_DS(){
         ES = 0;
         MC = 0;
        MapNode= new HashMap<>();
     }
+
     @Override
     public node_data getNode(int key) {
         if (this.MapNode.containsKey(key)) {
@@ -20,11 +23,13 @@ public class Graph_DS implements graph {
         return null;
     }
 
+
     @Override
     public boolean hasEdge(int node1, int node2) {
        node_data curr = this.MapNode.get(node1);
        return curr.getNi().contains(this.MapNode.get(node2));
     }
+
 
     @Override
     public void addNode(node_data n) {
@@ -37,6 +42,7 @@ public class Graph_DS implements graph {
 
     @Override
     public void connect(int node1, int node2) {
+        // check if the two given nodes are in the graph
         if(node1!= node2 && this.MapNode.containsKey(node1)&& this.MapNode.containsKey(node2)){
             if(!this.MapNode.get(node1).getNi().contains(this.MapNode.get(node2))) {
                 this.MapNode.get(node1).addNi(this.MapNode.get(node2));
@@ -51,8 +57,7 @@ public class Graph_DS implements graph {
     public Collection<node_data> getV() {
        return  this.MapNode.values();
     }
-//        return this.nodeDataList;
-//    }
+
 
     @Override
     public Collection<node_data> getV(int node_id) {
@@ -63,9 +68,11 @@ public class Graph_DS implements graph {
 
     @Override
     public node_data removeNode(int key) {
+        //check if the given nodes in the graph
         if(!this.MapNode.containsKey(key)){
             return null;
         }
+        // delete the given node and delete him from all is neighbors
         node_data nodeDataToRemove = this.MapNode.get(key);
         for(node_data cur : nodeDataToRemove.getNi()){
                 cur.removeNode(nodeDataToRemove);
